c95c9b54b4fb6c4737af448852071c109bbbca4d
Houmam <houmam.wazzeh@bordatech.com>
2025-02-11 08:29:06 +0300

    Add circuit breaker to FrankfurterApiService

--- Frankfurter.Api/FrankfurterApiService.cs
+++ Frankfurter.Api/FrankfurterApiService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using Polly;
+using Polly.CircuitBreaker;
 using Polly.Retry;
 
 namespace Frankfurter.Api;
@@ -10,8 +11,9 @@ public class FrankfurterApiService : IFrankfurterApiService
     private readonly IFrankfurterApiClient _client;
 
     private readonly AsyncRetryPolicy _retryPolicy;
+    private readonly AsyncCircuitBreakerPolicy _circuitBreakerPolicy;
 
-    public FrankfurterApiService(ILogger<FrankfurterApiService> logger, 
+    public FrankfurterApiService(ILogger<FrankfurterApiService> logger,
         IFrankfurterApiClient client)
     {
         _logger = logger;
@@ -27,13 +29,25 @@ public class FrankfurterApiService : IFrankfurterApiService
                     _logger.LogWarning($"Retry {retryCount} encountered an error: {exception.Message}. Waiting {timeSpan} before next retry.");
                 });
 
+        _circuitBreakerPolicy = Policy
+            .Handle<Exception>()
+            .CircuitBreakerAsync(
+                2, // Break the circuit after 2 exceptions
+                TimeSpan.FromMinutes(1),
+                onBreak: (exception, breakDelay) =>
+                {
+                    _logger.LogWarning($"Circuit breaker triggered due to: {exception.Message}. Circuit will be open for {breakDelay}.");
+                },
+                onReset: () => _logger.LogInformation("Circuit breaker reset."),
+                onHalfOpen: () => _logger.LogInformation("Circuit breaker is half-open, next call is a trial.")
+            );
     }
 
     public Task<ForexResponse> GetRateAsync(string baseCurrency, string[] currencies)
     {
         var symbols = currencies.Length > 0 ? string.Join(",", currencies) : null;
 
-        return _retryPolicy.ExecuteAsync(() => _client.GetLatestRatesAsync(baseCurrency, symbols));
+        return CallApi(() => _client.GetLatestRatesAsync(baseCurrency, symbols));
     }
 
     public Task<ForexResponseByDate> GetHistoryAsync(string baseCurrency, DateOnly startDate, DateOnly? endDate, string[] currencies)
@@ -45,6 +59,11 @@ public class FrankfurterApiService : IFrankfurterApiService
             dateRange += $"{endDate:yyyy-MM-dd}";
         }
 
-        return _retryPolicy.ExecuteAsync(() => _client.GetHistoricalRatesAsync(baseCurrency, dateRange, symbols));
+        return CallApi(() => _client.GetHistoricalRatesAsync(baseCurrency, dateRange, symbols));
+    }
+
+    private Task<TResponse> CallApi<TResponse>(Func<Task<TResponse>> apiCall)
+    {
+        return _circuitBreakerPolicy.ExecuteAsync(() =>  _retryPolicy.ExecuteAsync(apiCall));
     }
 }
